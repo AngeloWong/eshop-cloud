c5b4418d703b8f845c23236b3c982135d3ef040a
Angelo <allrounder@qq.com>
2020-01-21 02:53:07 +0800

    解决Eureka开启Security安全验证后，服务注册不上去的问题

--- eureka-server/src/main/java/com/angelo/eshop/EurekaServer.java
+++ eureka-server/src/main/java/com/angelo/eshop/EurekaServer.java
@@ -3,6 +3,9 @@ package com.angelo.eshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -10,5 +13,15 @@ public class EurekaServer {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer.class, args);
+    }
+
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+//            http.csrf().ignoringAntMatchers("/eureka/**");
+//            super.configure(http);
+        }
     }
 }
