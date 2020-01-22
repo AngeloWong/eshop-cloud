34a0e9300a3f4ddf81659f62f48646b1c1b523bb
Angelo <allrounder@qq.com>
2020-01-23 01:22:25 +0800

    数据同步服务

--- eshop-product-service/src/main/java/com/angelo/eshop/product/web/controller/BrandController.java
+++ eshop-product-service/src/main/java/com/angelo/eshop/product/web/controller/BrandController.java
@@ -4,7 +4,6 @@ import com.angelo.eshop.product.model.Brand;
 import com.angelo.eshop.product.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
